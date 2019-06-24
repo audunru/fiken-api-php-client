1ab089998aa979488c98b105dfc5a6fc147cc7ea
Audun Rundberg <audun@netliferesearch.com>
2019-06-25 00:07:07 +0200

    override toString method in Exception to return only the actual error

--- src/Exceptions/FikenClientException.php
+++ src/Exceptions/FikenClientException.php
@@ -6,8 +6,17 @@ use Exception;
 
 class FikenClientException extends Exception
 {
+    protected $error = [];
+
     public function __construct(string $message = '', int $code = 0)
     {
+        $this->error = json_decode($message, true)[0];
+
         parent::__construct($message, $code);
+    }
+
+    public function __toString(): string
+    {
+        return $this->error['message'];
     }
 }
